4cb22fa8b4a26dba554650a155877ef55c545ef2
vivek sangwan <viveksangwan006@gmail.com>
2021-06-03 15:35:44 +0530

    node

--- static/script.js
+++ static/script.js
@@ -17,16 +17,16 @@ for (var i = 0; i < themeDots.length; i++) {
 
 function setTheme(mode) {
     if (mode == "light") {
-        document.getElementById('theme-style').href = "default.css";
+        document.getElementById('theme-style').href = "./css/default.css";
     }
     if (mode == "green") {
-        document.getElementById('theme-style').href = "green.css";
+        document.getElementById('theme-style').href = "./css/green.css";
     }
     if (mode == "purple") {
-        document.getElementById('theme-style').href = "purple.css";
+        document.getElementById('theme-style').href = "./css/purple.css";
     }
     if (mode == "blue") {
-        document.getElementById('theme-style').href = "blue.css";
+        document.getElementById('theme-style').href = "./css/blue.css";
     }
 
     localStorage.setItem('theme', mode);
